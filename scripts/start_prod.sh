84ba8493849df621c8d90a8ca7f08f06659c39f1
Junhong <raimonfuns@gmail.com>
2020-02-08 11:40:25 +0800

    tweaks: add scripts

---
+++ scripts/start_prod.sh
@@ -0,0 +1,7 @@
+docker-compose -f docker-compose.yml -f docker-compose.pub-posts.yml down --remove-orphans &&
+
+echo " ------------- hard code --------------- "
+rm -rf postgresql &&
+rm -rf redis &&
+
+docker-compose -f docker-compose.yml -f docker-compose.pub-posts.yml up -d
