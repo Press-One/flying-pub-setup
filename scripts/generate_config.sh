0517958d2278ca824185e46b7786becf99506bf0
raimonfuns <raimonfuns@163.com>
2020-02-13 17:25:36 +0800

    tweak: login docker before generating config

--- scripts/generate_config.sh
+++ scripts/generate_config.sh
@@ -1,1 +1,2 @@
+docker login -u prs-os -p pressone dockerhub.qingcloud.com
 docker run -it --rm -v $(pwd)/config:/app/config dockerhub.qingcloud.com/pressone/flying-pub-config-generator sh -c "yarn generate_config"
