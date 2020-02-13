e95c93935e0c08203ebb0517918855969765b902
raimonfuns <raimonfuns@163.com>
2020-02-13 21:07:51 +0800

    tweak: pull images when docker-compose up

--- scripts/start_prod.sh
+++ scripts/start_prod.sh
@@ -1,2 +1,3 @@
+./scripts/pull_images.sh
 docker-compose -f docker-compose.yml -f docker-compose.pub-reader.yml down --remove-orphans &&
 docker-compose -f docker-compose.yml -f docker-compose.pub-reader.yml up -d
