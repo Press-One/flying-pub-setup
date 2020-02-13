5575b4cdd2cced9ae1f1d201680b64dd4136a58a
raimonfuns <raimonfuns@163.com>
2020-02-13 15:36:30 +0800

    tweak: remove codes about cleaning db and redis data

--- scripts/start_prod.sh
+++ scripts/start_prod.sh
@@ -1,7 +1,2 @@
 docker-compose -f docker-compose.yml -f docker-compose.pub-reader.yml down --remove-orphans &&
-
-echo " ------------- hard code --------------- "
-rm -rf postgresql &&
-rm -rf redis &&
-
 docker-compose -f docker-compose.yml -f docker-compose.pub-reader.yml up -d
