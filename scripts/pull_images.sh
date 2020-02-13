41e25203812df485440c240fa01802164e9aa418
raimonfuns <raimonfuns@163.com>
2020-02-13 21:27:15 +0800

    tweak: revert pull images

--- scripts/pull_images.sh
+++
@@ -1,6 +0,0 @@
-docker login -u prs-os -p pressone dockerhub.qingcloud.com
-docker pull dockerhub.qingcloud.com/pressone/atom
-docker pull dockerhub.qingcloud.com/pressone/postgres
-docker pull dockerhub.qingcloud.com/pressone/redis:alpine
-docker pull dockerhub.qingcloud.com/pressone/pub
-docker pull dockerhub.qingcloud.com/pressone/reader
--- scripts/start_prod.sh
+++ scripts/start_prod.sh
@@ -1,3 +1,2 @@
-./scripts/pull_images.sh
 docker-compose -f docker-compose.yml -f docker-compose.pub-reader.yml down --remove-orphans &&
 docker-compose -f docker-compose.yml -f docker-compose.pub-reader.yml up -d
