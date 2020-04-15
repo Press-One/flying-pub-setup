9e716bb9bd100b0bc893d69e7b3bf483e742d071
raimonfuns <raimonfuns@163.com>
2020-04-15 14:04:26 +0800

    feat(scripts): pull images before starting

--- scripts/pull_images.sh
+++
@@ -1,4 +0,0 @@
-docker login -u prs-os -p pressone dockerhub.qingcloud.com
-docker pull dockerhub.qingcloud.com/pressone/pub
-docker pull dockerhub.qingcloud.com/pressone/reader
-docker pull dockerhub.qingcloud.com/pressone/atom
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,7 @@
+docker login -u prs-os -p pressone dockerhub.qingcloud.com
+docker pull dockerhub.qingcloud.com/pressone/pub
+docker pull dockerhub.qingcloud.com/pressone/reader
+docker pull dockerhub.qingcloud.com/pressone/atom
+
 docker-compose -f docker-compose.yml -f docker-compose.pub-reader.yml down --remove-orphans &&
 docker-compose up -d
