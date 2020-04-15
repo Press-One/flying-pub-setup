fc642a418d244cac778313adb109f82b75085830
raimonfuns <raimonfuns@163.com>
2020-04-15 14:01:57 +0800

    feat(scripts): add pull_images.sh

---
+++ scripts/pull_images.sh
@@ -0,0 +1,4 @@
+docker login -u prs-os -p pressone dockerhub.qingcloud.com
+docker pull dockerhub.qingcloud.com/pressone/pub
+docker pull dockerhub.qingcloud.com/pressone/reader
+docker pull dockerhub.qingcloud.com/pressone/atom
