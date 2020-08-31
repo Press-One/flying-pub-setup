1b3101fc70ee1eefe216359ee8aeb898af6407c3
raimonfuns <raimonfuns@163.com>
2020-08-31 16:22:15 +0800

    tweak(start.sh): remove pub and reader config

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,5 @@
 docker login -u prs-os -p pressone dockerhub.qingcloud.com
-docker pull dockerhub.qingcloud.com/pressone/pub
-docker pull dockerhub.qingcloud.com/pressone/reader
 docker pull dockerhub.qingcloud.com/pressone/atom
 
-docker-compose -f docker-compose.yml -f docker-compose.pub-reader.yml down --remove-orphans &&
+docker-compose -f docker-compose.yml down --remove-orphans &&
 docker-compose up -d
