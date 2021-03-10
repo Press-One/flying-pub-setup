e33d18f579e57015cc1a826ef96777235ca30448
raimonfuns <raimonfuns@163.com>
2021-03-10 14:52:36 +0800

    tweak(flying-pub): tweak config

--- scripts/redeploy_flying_pub.sh
+++ scripts/redeploy_flying_pub.sh
@@ -1,1 +1,3 @@
+docker login -u prs-os -p pressone dockerhub.qingcloud.com
+docker pull dockerhub.qingcloud.com/pressone/flying-pub
 docker-compose up -d --force-recreate --no-deps flying-pub flying-pub-queue
