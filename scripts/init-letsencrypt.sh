8e74fc89e2b63edfa109893009679d5c27ab3635
raimonfuns <raimonfuns@163.com>
2021-03-19 11:34:10 +0800

    tweak ssl script

--- scripts/init-letsencrypt.sh
+++ scripts/init-letsencrypt.sh
@@ -15,7 +15,7 @@ staging=0 # Set to 1 if you're testing your setup to avoid hitting request limit
 
 for domain in ${domains[@]}; do
   echo $domain
-  if [ -d "$data_path/conf/live/$domain" ]; then
+  if [ -d "$data_path/conf/live/domain" ]; then
     read -p "Existing data found for $domain. Continue and replace existing certificate? (y/N) " decision
     if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
       exit
@@ -34,8 +34,8 @@ fi
 
 for domain in ${domains[@]}; do
   echo "### Creating dummy certificate for $domain ..."
-  path="/etc/letsencrypt/live/$domain"
-  mkdir -p "$data_path/conf/live/$domain"
+  path="/etc/letsencrypt/live/domain"
+  mkdir -p "$data_path/conf/live/domain"
   docker-compose run --rm --entrypoint "\
     openssl req -x509 -nodes -newkey rsa:2048 -days 1\
       -keyout '$path/privkey.pem' \
@@ -60,9 +60,9 @@ echo
 for domain in ${domains[@]}; do
   echo "### Deleting dummy certificate for $domain ..."
   docker-compose run --rm --entrypoint "\
-    rm -Rf /etc/letsencrypt/live/$domain && \
-    rm -Rf /etc/letsencrypt/archive/$domain && \
-    rm -Rf /etc/letsencrypt/renewal/$domain.conf" certbot
+    rm -Rf /etc/letsencrypt/live/domain && \
+    rm -Rf /etc/letsencrypt/archive/domain && \
+    rm -Rf /etc/letsencrypt/renewal/domain.conf" certbot
   echo
 done
 
