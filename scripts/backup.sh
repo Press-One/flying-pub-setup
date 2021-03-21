165d17e95d01514ca711474878ef3d0c735e9832
raimonfuns <raimonfuns@163.com>
2021-03-21 08:33:21 +0800

    tweak script

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -2,5 +2,4 @@ cd ..
 version=${1:-1}
 backup_folder_name="flying-pub-setup-backup-$version"
 cp -R flying-pub-setup "flying-pub-setup-backup-$version"
-echo "Created backup successfully!"
-echo "Folder: $(pwd)/$backup_folder_name"
+echo "Created backup successfully! -----> $(pwd)/$backup_folder_name"
