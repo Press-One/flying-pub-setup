c95e5972f03b28b72e3fcee6b9daf274070a7add
raimonfuns <raimonfuns@163.com>
2021-03-22 17:53:04 +0800

    tweak script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+
+config_file="$(pwd)/config/config.js"
+if [ ! -f $config_file ]; then
+    echo "$config_file not found"
+    exit
+fi
+
+config_wallet_file="$(pwd)/config/config.wallet.js"
+if [ ! -f $config_wallet_file ]; then
+    echo "$config_wallet_file not found"
+    exit
+fi
+
 ./scripts/init-letsencrypt.sh
