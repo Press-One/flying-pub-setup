0de2f726f6dc3afd8375e6994e0a9ac49dea559c
raimonfuns <raimonfuns@163.com>
2021-03-22 17:54:42 +0800

    tweak script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-config_file="$(pwd)/config/config.js"
+config_file="./config/config.js"
 if [ ! -f $config_file ]; then
     echo "$config_file not found"
     exit
 fi
 
-config_wallet_file="$(pwd)/config/config.wallet.js"
+config_wallet_file="./config/config.wallet.js"
 if [ ! -f $config_wallet_file ]; then
     echo "$config_wallet_file not found"
     exit
